2fc43d3206fecb47a6539923c181135e5eab311f
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-06-09 13:44:49 +0530

    Footer file created.

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-full text-center bg-transparent text-sm text-white">
+      <p>© 2025 Madhumita Y. Sugandhi. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
